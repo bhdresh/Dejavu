31e946e162df91db65e893449bb46dd7b642022b
H-DOS <78460904+hari-dos@users.noreply.github.com>
2021-07-25 13:32:11 -0400

    error_log disabled

--- Console/Decoify/includes/PHPMailer/src/PHPMailer.php
+++ Console/Decoify/includes/PHPMailer/src/PHPMailer.php
@@ -800,7 +800,7 @@ class PHPMailer
         switch ($this->Debugoutput) {
             case 'error_log':
                 //Don't output, just log
-                error_log($str);
+                //error_log($str);
                 break;
             case 'html':
                 //Cleans up output a bit for a better looking, HTML-safe output
