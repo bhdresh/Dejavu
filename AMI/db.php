d2f8cdcf676facece7bf7f5cd9ac5a8fae7cd82d
ll0_0ll <bhdresh@users.noreply.github.com>
2022-01-01 13:35:59 +0400

    Add files via upload

---
+++ AMI/db.php
@@ -0,0 +1,282 @@
+<?php
+
+//Connection to database
+
+function db_connect() {
+
+    // Define connection as a static variable, to avoid connecting more than once 
+    static $connection;
+
+    // Try and connect to the database
+    if(!isset($connection)) {
+        $config = parse_ini_file('config.ini'); 
+        $connection = mysqli_connect($config['host'],$config['username'],$config['password'],$config['dbname']);
+    }
+
+    // If connection was not successful, handle the error
+    if($connection === false) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    return $connection;
+}
+
+//Query the database
+
+function db_query($query) {
+    // Connect to the database
+    $connection = db_connect();
+
+    // Query the database
+    $result = mysqli_query($connection,$query);
+
+    return $result;
+}
+
+//Check Session
+
+function check_session(){
+
+  if(!isset($_SESSION)) 
+  { 
+      session_start(); 
+  }
+
+
+  if(isset($_SESSION['user_name']) &&  $_SESSION['role'] == 'admin')
+  {
+    $logged_in = 'true';
+    return $logged_in;
+  }
+
+  else
+  {
+    header('location:loginView.php');
+  }
+
+}
+
+// validate input -  string, numeric, space and . --> ^[A-Za-z0-9. ]*$
+
+function val_input($str)
+{
+
+  if(preg_match("/^[A-Za-z0-9.\- ]*$/", $str)) {
+    return true;
+  }
+
+  else{
+    return false; 
+  }
+
+}
+
+// Validate IP 
+
+function val_ip($str)
+{
+
+  if(preg_match('/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/',$str)) {
+    return true;
+  }
+
+  else{
+    return false;
+  }
+
+}
+
+// Data Filter
+
+function dataFilter($str)
+{
+  return htmlspecialchars($str, ENT_QUOTES);
+}
+
+//CheckAPI Connection
+
+function check_alive() {
+
+  $Console_IP = GetConsole_IP();
+
+  $url = "https://".$Console_IP."/Decoify/connection_check.php";
+
+  $apikey = GetApiKey();
+  $deviceid = GetDeviceID();
+
+  //new code
+
+  $data = array('auth_key' => $apikey, 'deviceid' => $deviceid);
+
+	$options = array(
+		"ssl"=>array(
+     		"verify_peer"=>false,
+      		"verify_peer_name"=>false,
+    		),
+		'http' => array(
+			'header'  => "Content-type: application/x-www-form-urlencoded\r\nConnection: close\r\n",
+      'method'  => 'POST',
+      'content' => http_build_query($data),
+      'timeout' => 2
+    )
+	);
+
+  $context  = stream_context_create($options);
+
+  $result = file_get_contents($url, false, $context);
+ 
+  return $result;
+}
+
+//Check number of events
+
+function check_activeevents() {
+
+  $Console_IP = GetConsole_IP();
+
+  $url = "https://".$Console_IP."/Decoify/getEventCount.php";
+
+  $apikey = GetApiKey();
+
+  //new code
+
+  $data = array('auth_key' => $apikey);
+
+	$options = array(
+		"ssl"=>array(
+                "verify_peer"=>false,
+                "verify_peer_name"=>false,
+                ),
+		'http' => array(
+			'header'  => "Content-type: application/x-www-form-urlencoded\r\nConnection: close\r\n",
+      'method'  => 'POST',
+      'content' => http_build_query($data),
+      'timeout' => 2
+    )
+	);
+
+  $context  = stream_context_create($options);
+
+  $result = file_get_contents($url, false, $context);
+  
+  return $result;
+}
+
+//CheckAPI Connection
+
+function check_api_connection() {
+
+  $Console_IP = GetConsole_IP();
+
+  $url = "https://".$Console_IP."/Decoify/connection_check.php";
+
+  //new code
+
+  $data = array('auth_key' => $apikey, 'deviceid' => $deviceid);
+
+	$options = array(
+		"ssl"=>array(
+                "verify_peer"=>false,
+                "verify_peer_name"=>false,
+                ),
+		'http' => array(
+			'header'  => "Content-type: application/x-www-form-urlencoded\r\nConnection: close\r\n",
+      'method'  => 'POST',
+      'content' => http_build_query($data),
+      'timeout' => 2
+    )
+	);
+
+  $context  = stream_context_create($options);
+
+  $result = file_get_contents($url, false, $context);
+  
+  return $result;
+}
+
+//Get API Key
+
+function GetApiKey()
+{
+  $mysqli = db_connect();
+
+  $stmt = $mysqli->prepare("SELECT * FROM Users Limit 1;");
+
+  $stmt->execute();
+
+  $result = $stmt->get_result();
+
+  while($row = $result->fetch_assoc()) {
+    $event[] = $row;
+  }
+
+  $apikey = $event[0]['amikey'];
+
+  return $apikey;
+}
+
+//Get Console IP
+
+function GetConsole_IP()
+{
+  $Console_IP = "cloudengine.camolabs.io";
+
+  return $Console_IP;
+}
+
+function GetDeviceID()
+{
+	  $mysqli = db_connect();
+          $stmt = $mysqli->prepare("SELECT * FROM Users");
+          $stmt->execute();
+          $result = $stmt->get_result();
+	  $row = $result->fetch_assoc();
+	  $deviceid=$row["deviceid"];
+
+	  $stmt->close();
+	return $deviceid;
+}
+
+//Get API Status
+
+function GetApiStatus()
+{
+  $mysqli = db_connect();
+
+  $stmt = $mysqli->prepare("SELECT * FROM Users Limit 1;");
+
+  $stmt->execute();
+
+  $result = $stmt->get_result();
+
+  while($row = $result->fetch_assoc()) {
+    $event[] = $row;
+  }
+
+  $apikey = $event[0]['amikey'];
+
+  $apikey="1";
+
+  return $apikey;
+}
+
+function SyslogStatus()
+{
+  $mysqli = db_connect();
+
+  $stmt = $mysqli->prepare("SELECT Status FROM SyslogDetails Limit 1;");
+
+  $stmt->execute();
+
+  $result = $stmt->get_result();
+
+  while($row = $result->fetch_assoc()) {
+    $event[] = $row;
+  }
+
+  $SyslogStatus = $event[0]['Status'];
+
+  return $SyslogStatus;
+}
+
+?>
+
---
+++ AMI/mailAlert.php
@@ -0,0 +1,244 @@
+<?php
+
+include 'db.php';
+
+//Retrieve deviceid
+
+$deviceid=GetDeviceID();
+
+//Get API key (apikey = amikey)
+
+$apikey = GetApiKey();
+
+//Get API status
+
+$apiStatus = GetApiStatus();
+$SyslogStatus = SyslogStatus();
+
+$Console_IP = GetConsole_IP();
+
+$url = 'https://'.$Console_IP.'/Decoify/collector.php';
+
+$uploadurl = 'https://'.$Console_IP.'/Decoify/fileupload.php';
+
+$logtime = date("Y-m-d H:i:s");
+
+function saveLog($decoyName, $decoyGroup, $decoyIP, $attackerIP, $eventType, $eventCategory, $timestamp, $rawlog, $serviceType, $decoy_type, $pcap_filename, $video_filename, $msg_filename)
+{
+	$mysqli = db_connect();
+
+	$status = 1;
+
+	$stmt = $mysqli->prepare("Insert Into MasterLogs (Decoy_Name, Decoy_Group, Decoy_IP, Attacker_IP, Service_Name,EventType, EventCategory, pcap_filename, video_filename, msg_filename, Raw_Logs, LogInsertedTimeStamp, decoy_type) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+
+	if (!$stmt) {
+    	throw new Exception('Error in preparing statement: ' . $mysqli->error);
+    	exit();
+	}
+
+	$stmt->bind_param("ssssssssssssi", $decoyName, $decoyGroup, $decoyIP, $attackerIP, $serviceType, $eventType, $eventCategory, $pcap_filename, $video_filename, $msg_filename, $rawlog,$logtime,$decoy_type);
+
+	$stmt->execute();
+
+	$stmt->close();
+
+}
+
+// Get all the values
+
+$val = getopt(null, ["decoyName:", "decoyGroup:", "decoyIP:", "attackerIP:", "eventType:", "eventCategory:","timestamp:", "rawlog:", "serviceType:", "decoyType:", "pcap_filename:", "video_filename:", "msg_filename:"]);
+
+$decoyName = $val['decoyName'];
+$decoyGroup = $val['decoyGroup'];
+$decoyIP = $val['decoyIP'];
+$attackerIP = $val['attackerIP'];
+$eventType = $val['eventType'];
+$eventCategory = $val['eventCategory'];
+$timestamp = $val['timestamp'];
+$rawlog = $val['rawlog'];
+$serviceType = $val['serviceType'];
+$decoy_type = $val['decoyType'];
+$pcap_filename = $val['pcap_filename'];
+$video_filename= $val['video_filename'];
+$msg_filename= $val['msg_filename'];
+
+
+if($apiStatus == 1)
+{
+
+	//check for connectivity with server
+
+
+	if(check_alive() == 'Valid_Key')
+	{	
+
+		//Send current log
+
+
+		$data = array('auth_key' => $apikey, 'deviceid' => $deviceid, 'Decoy_Name' => $decoyName, 'Decoy_Group' => $decoyGroup, 'Decoy_IP' => $decoyIP,'Attacker_IP' => $attackerIP, 'EventType' => $eventType, 'Raw_Logs' => $rawlog, 'Service_Name' => $serviceType, 'Timestamp' => $logtime, 'decoy_type' => $decoy_type, 'pcap_filename' => $pcap_filename, 'video_filename' => $video_filename, 'msg_filename' => $msg_filename);
+
+		$options = array(
+			'http' => array(
+				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+		'method'  => 'POST',
+				'content' => http_build_query($data)
+		),
+		"ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false)
+		);
+
+		$context  = stream_context_create($options);
+		//echo $context;
+
+		$result = file_get_contents($url, false, $context);
+
+		if ($result === FALSE) { /* Handle error */ }
+
+
+
+		//Upload pcap file
+
+		if (!empty($pcap_filename))
+		{
+
+		define('MULTIPART_BOUNDARY', '--------------------------'.microtime(true));
+		$header = 'Content-Type: multipart/form-data; boundary='.MULTIPART_BOUNDARY;
+		define('FORM_FIELD', 'uploaded_file'); 
+		$filename = "/var/dejavufiles/captures/$pcap_filename";
+		$file_contents = file_get_contents($filename);    
+
+		$content =  "--".MULTIPART_BOUNDARY."\r\n".
+            	"Content-Disposition: form-data; name=\"".FORM_FIELD."\"; filename=\"".basename($filename)."\"\r\n".
+            	"Content-Type: application/octet-stream\r\n\r\n".
+            	$file_contents."\r\n";
+
+		// add some POST fields to the request too: $_POST['foo'] = 'bar'
+		$content .= "--".MULTIPART_BOUNDARY."\r\n".
+            	"Content-Disposition: form-data; name=\"auth_key\"\r\n\r\n".
+		"$apikey\r\n";
+
+		// add some POST fields to the request too: $_POST['foo'] = 'bar'
+                $content .= "--".MULTIPART_BOUNDARY."\r\n".
+                "Content-Disposition: form-data; name=\"deviceid\"\r\n\r\n".
+                "$deviceid\r\n";
+
+		// signal end of request (note the trailing "--")
+		$content .= "--".MULTIPART_BOUNDARY."--\r\n";
+		$context = stream_context_create(array(
+    			'http' => array(
+          		'method' => 'POST',
+          		'header' => $header,
+          		'content' => $content,
+    			),
+			"ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false)
+		));
+		$result = file_get_contents($uploadurl, false, $context);
+
+		print_r($result);
+
+		unlink("/var/dejavufiles/captures/$pcap_filename");
+
+		}
+
+		//Upload video file
+
+		if (!empty($video_filename))
+                {
+
+                define('MULTIPART_BOUNDARY', '--------------------------'.microtime(true));
+                $header = 'Content-Type: multipart/form-data; boundary='.MULTIPART_BOUNDARY;
+                define('FORM_FIELD', 'uploaded_file');
+                $filename = "/var/dejavufiles/captures/$video_filename";
+                $file_contents = file_get_contents($filename);
+
+                $content =  "--".MULTIPART_BOUNDARY."\r\n".
+                "Content-Disposition: form-data; name=\"".FORM_FIELD."\"; filename=\"".basename($filename)."\"\r\n".
+                "Content-Type: video/mp4\r\n\r\n".
+                $file_contents."\r\n";
+
+                // add some POST fields to the request too: $_POST['foo'] = 'bar'
+                $content .= "--".MULTIPART_BOUNDARY."\r\n".
+                "Content-Disposition: form-data; name=\"auth_key\"\r\n\r\n".
+		"$apikey\r\n";
+
+		// add some POST fields to the request too: $_POST['foo'] = 'bar'
+                $content .= "--".MULTIPART_BOUNDARY."\r\n".
+                "Content-Disposition: form-data; name=\"deviceid\"\r\n\r\n".
+                "$deviceid\r\n";
+
+
+                // signal end of request (note the trailing "--")
+                $content .= "--".MULTIPART_BOUNDARY."--\r\n";
+                $context = stream_context_create(array(
+                        'http' => array(
+                        'method' => 'POST',
+                        'header' => $header,
+                        'content' => $content,
+                        ),
+			"ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false)
+                ));
+                $result = file_get_contents($uploadurl, false, $context);
+                unlink("/var/dejavufiles/captures/$video_filename");
+
+                }	
+
+
+		//Upload msg file
+
+                if (!empty($msg_filename))
+                {
+
+                define('MULTIPART_BOUNDARY', '--------------------------'.microtime(true));
+                $header = 'Content-Type: multipart/form-data; boundary='.MULTIPART_BOUNDARY;
+                define('FORM_FIELD', 'uploaded_file');
+                $filename = "/var/dejavufiles/captures/$msg_filename";
+                $file_contents = file_get_contents($filename);
+
+                $content =  "--".MULTIPART_BOUNDARY."\r\n".
+                "Content-Disposition: form-data; name=\"".FORM_FIELD."\"; filename=\"".basename($filename)."\"\r\n".
+                "Content-Type: message/rfc822\r\n\r\n".
+                $file_contents."\r\n";
+
+                // add some POST fields to the request too: $_POST['foo'] = 'bar'
+                $content .= "--".MULTIPART_BOUNDARY."\r\n".
+                "Content-Disposition: form-data; name=\"auth_key\"\r\n\r\n".
+		"$apikey\r\n";
+
+		// add some POST fields to the request too: $_POST['foo'] = 'bar'
+                $content .= "--".MULTIPART_BOUNDARY."\r\n".
+                "Content-Disposition: form-data; name=\"deviceid\"\r\n\r\n".
+                "$deviceid\r\n";
+
+
+                // signal end of request (note the trailing "--")
+                $content .= "--".MULTIPART_BOUNDARY."--\r\n";
+                $context = stream_context_create(array(
+                        'http' => array(
+                        'method' => 'POST',
+                        'header' => $header,
+                        'content' => $content,
+                        ),
+                        "ssl"=>array("verify_peer"=>false,"verify_peer_name"=>false)
+                ));
+                $result = file_get_contents($uploadurl, false, $context);
+                unlink("/var/dejavufiles/captures/$msg_filename");
+
+                }      
+
+
+	}
+
+	else{
+		saveLog($decoyName, $decoyGroup, $decoyIP, $attackerIP, $eventType, $eventCategory, $timestamp, $rawlog, $serviceType, $decoy_type, $pcap_filename, $video_filename, $msg_filename);
+	}
+
+}
+
+if($SyslogStatus == 1)
+{
+	$syslog = $decoyName." | ".$decoyGroup." | ".$decoyIP." | ".$serviceType." | ".$decoy_type." | ".$attackerIP." | ".$eventType." | ".$logtime." | ".$pcap_filename." | ".$video_filename." | ".$msg_filename;
+	
+	$myfile = file_put_contents('/var/log/syslogclient.log', $syslog.PHP_EOL , FILE_APPEND | LOCK_EX);
+}
+
+?>
+
