aa937b3817503e63b9ef15c4fa035a0eae67206b
ll0_0ll <bhdresh@users.noreply.github.com>
2022-05-13 20:52:17 +0400

    Render latest available version
    Render latest available version and removed file upload from client side

--- Engine/Decoify/backupSettings.php
+++ Engine/Decoify/backupSettings.php
@@ -93,23 +93,24 @@ if(isset($_SESSION['user_name']) && $_SESSION['role'] == 'admin') {
 
         <div class="box box-primary">
         <div class="box-header with-border">
-          <h3 class="box-title">Upgrade DejaVu (Current Version : 
-            <?php 
-            $config = parse_ini_file('config/config.ini'); 
-            echo $config['currentVersion'];
-            ?>
-          )
-      </h3>
+          <h3 class="box-title">Upgrade DejaVu</h3>
         </div>
         <!-- /.box-header -->
         <div class="box-body">
+
+	<?php
+		$latestversion = `curl "https://camolabs.io/upgrade/upgrade.php?latestversion=check"`;
+            	$config = parse_ini_file('config/config.ini');
+		$currentversion=$config['currentVersion'];;
+
+		echo "<label>Current Version : <p style=\"color:blue\">$currentversion</p> </label>";
+		echo "<br>";
+		echo "<label>Latest Version : <p style=\"color:green\">$latestversion</p> </label>";
+	?>
+
           
             <form role="form" action="updateFramework.php" method="post" enctype="multipart/form-data">
 		<input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>"/>
-                <div class="form-group">
-                     <input type="hidden" name="file_true" value="1">
-                     <input type="file" name="fileToUpload" id="fileToUpload">
-                </div>
                 <button type="submit" class="btn btn-primary">Upgrade DejaVu Engine</button>
             </form>
           </div>
@@ -117,6 +118,7 @@ if(isset($_SESSION['user_name']) && $_SESSION['role'] == 'admin') {
           </div>
 
         </div>
+
 
 
 
